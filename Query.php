42f5677184878e36f8a9bb0fa63e6a1a73e68a7e
florianhumblot <31630192+florianhumblot@users.noreply.github.com>
2018-01-24 14:49:14 +0100

    Update Query.php

--- Query.php
+++ Query.php
@@ -8,4 +8,12 @@ function Query($sql, $args = null, $fetch = true, $numrow = false, $lastid = fal
             $query->bindValue(":" . $key, $value);
         }
     }
+    if ($fetch)
+        $ret = $query->fetchAll();
+    elseif ($numrow)
+        $ret = $query->rowCount();
+    elseif ($lastid)
+        $ret = $db->lastInsertId();
+    
+    return $ret;
 }
